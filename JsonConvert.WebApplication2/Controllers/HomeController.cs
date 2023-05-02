f8921f7d28c9423662f094d2d9afcefa39051838
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-02 09:56:43 +0430

    Add project files.

---
+++ JsonConvert.WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Newton = Newtonsoft.Json;
+
+namespace JsonConvert.WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var json = "[\"philcollins\",\"Ih8PeterG\"]";
+       
+            var searchFilters = json;
+            var output = Newton.JsonConvert.DeserializeObject<string[]>(searchFilters);
+            var lst = output.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var json = "[{ \"firstName\":\"John\", \"lastName\":\"Doe\"},]";
+            var searchFilters = json;
+            var output = Newton.JsonConvert.DeserializeObject<KeyValuePair<string, string>[]>(searchFilters);
+            var lst = output.ToList();
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ JsonConvert.WebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JsonConvert.WebApplication2.MvcApplication" Language="C#" %>
